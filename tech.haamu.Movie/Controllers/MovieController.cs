66cbef4eed3a243257548fdf474253920466e33f
Jussi Raunio <jussi.raunio@gmail.com>
2020-08-08 22:53:15 +0300

    Add project files.

---
+++ tech.haamu.Movie/Controllers/MovieController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tech.haamu.Movie.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        public Task<string> Index()
+        {
+            return Task.FromResult("Hello World!");
+        }
+    }
+}
