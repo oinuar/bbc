07cc1f93449030b3497182adb906b27b5804f145
Jussi Raunio <jussi.raunio@gmail.com>
2020-08-09 11:45:37 +0300

    Add IMDBMovieLibrary.

---
+++ tech.haamu.Movie.UnitTest/Services/IMDBMovieLibraryTest.cs
@@ -0,0 +1,45 @@
+﻿using tech.haamu.Movie.Services;
+using Moq;
+using System.Collections.Immutable;
+using System.Linq;
+using Xunit;
+
+namespace tech.haamu.Movie.UnitTest.Services
+{
+    public class IMDBMovieLibraryTest
+    {
+        [Fact]
+        public void IMDBMovieLibrary()
+        {
+            var movies = new Mock<IImmutableSet<Movie.Models.Movie>>(MockBehavior.Strict);
+
+            movies
+                .Setup(x => x.Add(It.Is<Movie.Models.Movie>(y => y.Id == "tt0111161" && new[] { "Drama" }.SequenceEqual(y.Genres) && y.Name == "The Shawshank Redemption")))
+                .Returns(movies.Object);
+
+            movies
+                .Setup(x => x.Add(It.Is<Movie.Models.Movie>(y => y.Id == "tt0068646" && new[] { "Crime", "Drama" }.SequenceEqual(y.Genres) && y.Name == "The Godfather")))
+                .Returns(movies.Object);
+
+            movies
+                .Setup(x => x.Add(It.Is<Movie.Models.Movie>(y => y.Id == "tt0071562" && new[] { "Crime", "Drama" }.SequenceEqual(y.Genres) && y.Name == "The Godfather: Part II")))
+                .Returns(movies.Object);
+
+            movies
+                .Setup(x => x.Add(It.Is<Movie.Models.Movie>(y => y.Id == "tt0468569" && new[] { "Action", "Crime", "Drama" }.SequenceEqual(y.Genres) && y.Name == "The Dark Knight")))
+                .Returns(movies.Object);
+
+            movies
+                .Setup(x => x.Add(It.Is<Movie.Models.Movie>(y => y.Id == "tt0050083" && new[] { "Crime", "Drama" }.SequenceEqual(y.Genres) && y.Name == "12 Angry Men")))
+                .Returns(movies.Object);
+
+            movies
+                .Setup(x => x.Add(It.Is<Movie.Models.Movie>(y => y.Id == "tt0108052" && new[] { "Biography", "Drama", "History" }.SequenceEqual(y.Genres) && y.Name == "Schindler's List")))
+                .Returns(movies.Object);
+
+            var movieLibrary = new IMDBMovieLibrary(movies.Object);
+
+            movies.VerifyAll();
+        }
+    }
+}
---
+++ tech.haamu.Movie/Services/IMDBMovieLibrary.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace tech.haamu.Movie.Services
+{
+    // TODO: remove InMemoryLibrary base and implement IMovieLibrary directly to use real IMDB's API.
+    public class IMDBMovieLibrary : InMemoryMovieLibrary
+    {
+        public IMDBMovieLibrary(IImmutableSet<Models.Movie> emptySet) : base(emptySet
+            .Add(new Models.Movie { Id = "tt0111161", Genres = new[] { "Drama" }, Name = "The Shawshank Redemption" })
+            .Add(new Models.Movie { Id = "tt0068646", Genres = new[] { "Crime", "Drama" }, Name = "The Godfather" })
+            .Add(new Models.Movie { Id = "tt0071562", Genres = new[] { "Crime", "Drama" }, Name = "The Godfather: Part II" })
+            .Add(new Models.Movie { Id = "tt0468569", Genres = new[] { "Action", "Crime", "Drama" }, Name = "The Dark Knight" })
+            .Add(new Models.Movie { Id = "tt0050083", Genres = new[] { "Crime", "Drama" }, Name = "12 Angry Men" })
+            .Add(new Models.Movie { Id = "tt0108052", Genres = new[] { "Biography", "Drama", "History" }, Name = "Schindler's List" }))
+        {
+        }
+    }
+}
